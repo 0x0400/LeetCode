314470d06da76b18015fa78e497681d02b37c48f
Xingang Zhang <0x0400@users.noreply.github.com>
2021-04-09 11:35:45 +0800

    problem 142: linked-list-cycle-ii

---
+++ p142.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/linked-list-cycle-ii/
+
+from common.linked_list import ListNode
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        visited = {}
+        idx = 0
+        curNode = head
+        while curNode:
+            if curNode in visited:
+                return curNode
+
+            visited[curNode] = idx
+            idx += 1
+            curNode = curNode.next
+
+        return None
