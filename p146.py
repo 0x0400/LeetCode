49481cda40f66cfed80d3c237d2779f5a34a492a
Xingang Zhang <0x0400@users.noreply.github.com>
2021-04-12 17:06:08 +0800

    problem 146: 双向链表实现

--- p146.py
+++ p146.py
@@ -1,4 +1,7 @@
 # https://leetcode.com/problems/lru-cache/
+from typing import Dict
+
+
 class LRUCache:
 
     def __init__(self, capacity: int):
@@ -38,3 +41,71 @@ class LRUCache:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
 # obj.put(key,value)
+
+
+class Node:
+    def __init__(self, key: int, val: int, prev: 'Node' = None, next: 'Node' = None) -> None:
+        self.key = key
+        self.val = val
+        self.prev = prev
+        self.next = next
+
+
+class LRUCacheV2:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.keys : Dict[int, Node] = {}
+        self.head: Node = None
+        self.last: Node = None
+
+    def _add(self, node: Node):
+        node.prev = None
+        node.next = self.head
+        if self.head:
+            self.head.prev = node
+        self.head = node
+
+        if self.last is None:
+            self.last = node
+            self.last.next = None
+
+    def _remove(self, node: Node):
+        if self.head == node:
+            self.head = node.next
+            node.next = None
+            return
+
+        if self.last == node:
+            self.last = node.prev
+            if self.last:
+                self.last.next = None
+            return
+
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+    def get(self, key: int) -> int:
+        if key not in self.keys:
+            return -1
+
+        node = self.keys.get(key)
+        self._remove(node)
+        self._add(node)
+        return node.val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.keys:
+            node = self.keys[key]
+            node.val = value
+            self._remove(node)
+            self._add(node)
+            return
+
+        node = Node(key, value)
+        self.keys[key] = node
+        self._add(node)
+
+        if len(self.keys) > self.capacity:
+            del self.keys[self.last.key]
+            self._remove(self.last)
--- p146_test.py
+++ p146_test.py
@@ -1,15 +1,15 @@
-from p146 import LRUCache
+from p146 import LRUCache, LRUCacheV2
 
 test_cases = [
     ((["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"], [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]), [None, None, None, 1, None, -1, None, -1, 3, 4])
 ]
 
-def runCase(actions, params):
+def runCase(cls, actions, params):
     instacne = None
     rst = []
     for act, param in zip(actions, params):
         if act == "LRUCache":
-            instacne = LRUCache(*param)
+            instacne = cls(*param)
             rst.append(None)
             continue
         if not instacne:
@@ -26,4 +26,8 @@ def runCase(actions, params):
 
 def test_LRUCache():
     for case in test_cases:
-        assert runCase(*case[0]) == case[1], case
+        assert runCase(LRUCache, *case[0]) == case[1], case
+
+def test_LRUCacheV2():
+    for case in test_cases:
+        assert runCase(LRUCacheV2, *case[0]) == case[1], case
