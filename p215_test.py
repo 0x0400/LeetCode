2eca84ebb8e0e2da286622540be17925107fa954
Xingang Zhang <0x0400@users.noreply.github.com>
2021-05-12 17:32:58 +0800

    problem 215: kth-largest-element-in-an-array

---
+++ p215_test.py
@@ -0,0 +1,11 @@
+from p215 import Solution
+
+test_cases = [
+    [[[3,2,1,5,6,4], 2], 5],
+    [[[3,2,3,1,2,4,5,5,6], 4], 4],
+]
+
+def test_findKthLargest():
+    s = Solution()
+    for case in test_cases:
+        assert s.findKthLargest(*case[0]) == case[1], case
