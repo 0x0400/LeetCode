94bb9fedc908490cc52d87def317c057fadaeceb
Xingang Zhang <metadao@gmail.com>
2023-02-11 00:46:31 +0800

    improve problem 221

--- p221-maximal-square/p221.go
+++ p221-maximal-square/p221.go
@@ -1,10 +1,17 @@
 package p221
 
-func getMaxSquareLen(matrix [][]byte, rowSrc int, colSrc int, initMaxLen int) int {
+type Result struct {
+	maxLen int
+
+	nextColIdx     int
+	nextInitMaxLen int
+}
+
+func getMaxSquareLen(matrix [][]byte, rowSrc int, colSrc int, initMaxLen int) Result {
 	rowLen, colLen := len(matrix), len(matrix[0])
 
 	if rowSrc >= rowLen || colSrc >= colLen || matrix[rowSrc][colSrc] == '0' {
-		return 0
+		return Result{0, colSrc + 1, 0}
 	}
 
 	maxLen := 1
@@ -14,14 +21,14 @@ func getMaxSquareLen(matrix [][]byte, rowSrc int, colSrc int, initMaxLen int) in
 	for ; rowSrc+maxLen < rowLen && colSrc+maxLen < colLen; maxLen++ {
 		for i := 0; i <= maxLen; i++ {
 			if matrix[rowSrc+i][colSrc+maxLen] == '0' {
-				return maxLen
+				return Result{maxLen, colSrc + maxLen + 1, 0}
 			}
 			if matrix[rowSrc+maxLen][colSrc+i] == '0' {
-				return maxLen
+				return Result{maxLen, colSrc + i + 1, maxLen - i - 1}
 			}
 		}
 	}
-	return maxLen
+	return Result{maxLen, colSrc + maxLen, 0}
 }
 
 func maximalSquare(matrix [][]byte) int {
@@ -29,12 +36,13 @@ func maximalSquare(matrix [][]byte) int {
 	rowLen, colLen := len(matrix), len(matrix[0])
 	for rowIdx := 0; rowIdx+maxLen-1 < rowLen; rowIdx++ {
 		curMaxLen := 0
-		for colIdx := 0; colIdx+maxLen-1 < colLen; colIdx++ {
-			curMaxLen = getMaxSquareLen(matrix, rowIdx, colIdx, curMaxLen)
-			if curMaxLen > maxLen {
-				maxLen = curMaxLen
+		for colIdx := 0; colIdx+maxLen-1 < colLen; {
+			rst := getMaxSquareLen(matrix, rowIdx, colIdx, curMaxLen)
+			if rst.maxLen > maxLen {
+				maxLen = rst.maxLen
 			}
-			curMaxLen--
+			colIdx = rst.nextColIdx
+			curMaxLen = rst.nextInitMaxLen
 		}
 	}
 	return maxLen * maxLen
